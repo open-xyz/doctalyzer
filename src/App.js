08130ebd2d973464fe6733ddc8e77bd9a7a9b435
rohansx <sharmarohan615@gmail.com>
2023-04-21 22:52:26 +0530

    added ocr and gpt api

--- src/App.js
+++ src/App.js
@@ -4,15 +4,41 @@ import './App.css';
 
 function App() {
   const [reportText, setReportText] = useState('');
+  const [interpretedReportText, setInterpretedReportText] = useState('');
+  const [chatGPTApiKey, setChatGPTApiKey] = useState('sk-hpX8BXe5xJNSuUrImN53T3BlbkFJva5fvmNvBR1jRkHAPexuY');
 
   const handleFileSubmit = async (event) => {
     event.preventDefault();
+    console.log("Submit button clicked"); // add this line to see if the function is being called
+
     const file = event.target.elements.fileInput.files[0];
 
     // Use Tesseract.js to extract text from the uploaded image
     const { data: { text } } = await Tesseract.recognize(file);
 
     setReportText(text);
+  };
+
+  const handleInterpretationSubmit = async (event) => {
+    event.preventDefault();
+
+
+
+    // Make a request to the ChatGPT API to interpret the medical report in simple language
+    const response = await fetch('https://api.chatgpt.com/interpret', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${chatGPTApiKey}`,
+      },
+      body: JSON.stringify({
+        reportText,
+      }),
+    });
+
+    const data = await response.json();
+    setInterpretedReportText(data.interpretedReportText);
+    console.log(data);
   };
 
   return (
@@ -22,7 +48,13 @@ function App() {
         <input type="file" id="fileInput" />
         <button type="submit">Submit</button>
       </form>
-      {reportText && <p>{reportText}</p>}
+      {reportText && (
+        <form onSubmit={handleInterpretationSubmit}>
+          <textarea value={reportText} onChange={(event) => setReportText(event.target.value)} />
+          <button type="submit">Interpret</button>
+        </form>
+      )}
+      {interpretedReportText && <p>{interpretedReportText}</p>}
     </div>
   );
 }
