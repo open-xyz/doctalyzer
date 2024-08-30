6b9988fff8fab432869175df74e29fe7d853ac61
Shweta Kadam <shwetarkadam25@gmail.com>
2024-08-30 19:52:01 +0530

    Update Medicalreport.jsx

--- src/components/Medicalreport.jsx
+++ src/components/Medicalreport.jsx
@@ -103,4 +103,75 @@ function Medicalreport() {
 				<div className='flex flex-row justify-around mt-5'>
 					<div
 						onClick={handleMedicineClick}
-						className='cursor-pointer hover rounded-full bg-white border-solid border
+						className='cursor-pointer hover rounded-full bg-white border-solid border-2 border-orange-500 px-5 mx-2'
+					>
+						Combiflame
+					</div>
+					<div
+						onClick={handleMedicineClick}
+						className='cursor-pointer hover rounded-full bg-white border-solid border-2 border-orange-500 px-5 mx-2'
+					>
+						Diclofanac
+					</div>
+					<div className='orange_gradient px-2 pt-1 text-sm'>1M+ Medicines</div>
+				</div>
+			</div>
+
+			{/* //////Hero Ends */}
+
+			<div className='flex flex-col w-full items-center justify-center'>
+				<input
+					placeholder='Search for a medicine'
+					type='text'
+					className='w-full p-5 rounded-full border max-w-2xl '
+					value={inputMessage}
+					onChange={handleInputChange}
+				/>
+				<button
+					style={{ backgroundColor: isGenerating ? "grey" : "#eb5c0c" }}
+					onClick={convertImageToText}
+					className='my-5 border-gray-200 text-white flex h-10 w-full max-w-2xl items-center justify-center rounded-md border text-sm transition-all focus:outline-none'
+					disabled={isGenerating}
+				>
+					<p className='text-sm'>
+						{isGenerating ? "Generating..." : "Generate report"}
+					</p>
+				</button>
+			</div>
+			<div>
+				{error && <p>{error}</p>}
+				{resultJSON && (
+					<div className='max-w-2xl'>
+						<div className='my-5 p-5 rounded-md border bg-white'>
+							<h3 className='font-semibold text-lg mb-1'>Uses:</h3>
+							<p>{resultJSON.Uses}</p>
+						</div>
+
+						<div className='my-5 p-5 rounded-md border bg-white'>
+							<h3 className='font-semibold text-lg mb-1'>Dosage:</h3>
+							<p>{resultJSON.Dosage}</p>
+						</div>
+
+						<div className='my-5 p-5 rounded-md border bg-white'>
+							<h3 className='font-semibold text-lg mb-1'>Side Effects:</h3>
+							<p>{resultJSON["Side Effects"]}</p>
+						</div>
+
+						<div className='my-5 p-5 rounded-md border bg-white'>
+							<h3 className='font-semibold text-lg mb-1'>Route:</h3>
+							<p>{resultJSON.Route}</p>
+						</div>
+
+						<div className='my-5 p-5 rounded-md border bg-white'>
+							<h3 className='font-semibold text-lg mb-1'>Disclaimer:</h3>
+							<p>{resultJSON.Disclaimer}</p>
+						</div>
+					</div>
+				)}
+			</div>
+			<Footer />
+		</>
+	);
+}
+
+export default Medicalreport;
